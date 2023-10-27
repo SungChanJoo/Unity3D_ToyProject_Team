e8506029cde17ec7e5754d508d369f4c02b55698
yk170901 <yk170901@gmailcom>
2023-10-27 17:35:26 +0900

    [Feat] 랭킹 시스템

--- Assets/3.Script/System/UiManager.cs
+++ Assets/3.Script/System/UiManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -10,7 +10,11 @@ public class UiManager : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI _scoreTxt;
 
+    [SerializeField] private List<TextMeshProUGUI> _rankingScoreTxts;
+
     private float _startTime;
+
+    private int _curScore = -1;
 
     private bool _isGameOver = false;
 
@@ -18,7 +22,8 @@ public class UiManager : MonoBehaviour
     {
         if (_isGameOver) return;
 
-        _scoreTxt.text = $"Score : {(int)(Time.time - _startTime)}";
+        _curScore = (int)(Time.time - _startTime);
+        _scoreTxt.text = _curScore.ToString();
     }
 
     public void OnEnable()
@@ -31,5 +36,73 @@ public class UiManager : MonoBehaviour
     {
         _gameOverUi.SetActive(true);
         _isGameOver = true;
+
+        UpdateScoreRanking();
+        ShowScoreRanking();
+    }
+
+    private void UpdateScoreRanking()
+    {
+        List<int> rankingScores = FetchScoreHistory();
+
+        if (rankingScores.Count.Equals(0))
+        {
+            // RankingScore0에 0이 붙음에 주의
+            PlayerPrefs.SetInt($"RankingScore0", _curScore);
+            return;
+        }
+
+        bool isCurScoreStored = false;
+
+        int temp = -1;
+
+        for (int i = 0; i < rankingScores.Count; i++)
+        {
+            if (isCurScoreStored)
+            {
+                PlayerPrefs.SetInt($"RankingScore{i}", temp);
+                temp = rankingScores[i];
+
+                continue;
+            }
+
+            if (_curScore > rankingScores[i])
+            {
+                temp = rankingScores[i];
+                PlayerPrefs.SetInt($"RankingScore{i}", _curScore);
+
+                isCurScoreStored = true;
+                i--;
+            }
+        }
+
+        if(rankingScores.Count < _rankingScoreTxts.Count)
+            PlayerPrefs.SetInt($"RankingScore{rankingScores.Count}", _curScore);
+
+    }
+
+    private void ShowScoreRanking()
+    {
+        List<int> scores = FetchScoreHistory();
+
+        for (int i = 0; i < scores.Count; i++)
+            _rankingScoreTxts[i].text = scores[i].ToString();
+    }
+
+    private List<int> FetchScoreHistory()
+    {
+        List<int> scores = new List<int>();
+
+        for(int i =0; i < 3; i++)
+        {
+            int score = PlayerPrefs.GetInt($"RankingScore{i}", -1);
+
+            // 랭킹 점수가 더는 존재하지 않음
+            if (score.Equals(-1)) break;
+
+            scores.Add(score);
+        }
+
+        return scores;
     }
 }
